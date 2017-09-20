322a742a64b007d132afe40fc14cd39050f98948
Prudhvikothapalli <prudhvi.kothapalli@gmail.com>
2017-09-20 16:16:49 -0400

    code working properly for firefox

--- src/main/java/TestSelenium.java
+++ src/main/java/TestSelenium.java
@@ -3,12 +3,13 @@
 
 
 
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import javax.lang.model.element.Element;
+
 
 //comment the above line and uncomment below line to use Chrome
 //import org.openqa.selenium.chrome.ChromeDriver;
@@ -20,34 +21,30 @@ public class TestSelenium {
         //comment the above 2 lines and uncomment below 2 lines to use Chrome
         // System.setProperty("webdriver.chrome.driver","C:/Program Files/Java/chromedriver_win32/chromedriver.exe");
         // WebDriver driver = new ChromeDriver();
+        // goes to google.com web site
         driver.get("http://www.google.com");
-        String baseUrl = "https://www.google.com";
+        // finds search field
         WebElement element = driver.findElement(By.name("q"));
-
-        //WebElement element = driver.findElement(By.id("lit-ib"));
+        // enters value selenium
         element.sendKeys("Selenium");
         //Insert code here to locate and click on Search button
         driver.findElement(By.name("btnK")).click();
-        element.submit();
-        String expectedTitle = "Selenium - Web Browser Automation ";
-        String actualTitle = "";
 
-        // launch Fire fox and direct it to the Base URL
-        driver.get(baseUrl);
-
-        // get the actual value of the title
-        actualTitle = driver.getTitle();
 
         /*
          * compare the actual title of the page with the expected one and print
          * the result as "Passed" or "Failed"
          */
-        if (actualTitle.contentEquals(expectedTitle)){
+        //if (actualTitle.contentEquals(expectedTitle))
+        // finds the given string on the page
+        if(driver.getPageSource().contains("Selenium - Web Browser Automation"))
+        {
             System.out.println("Test Passed!");
         } else {
             System.out.println("Test Failed");
         }
-        Thread.sleep(5000);
+
+        Thread.sleep(10000);
         //close Fire fox
         driver.close();
     }
